7415807721605f77ac36d5e016dc8b86198b800d
Pranav Kurundodi <111072474+PranavKurundodi@users.noreply.github.com>
2024-04-14 23:52:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -23,6 +23,32 @@ func main() {
 	r.HandleFunc("/pokemon/byName", handlers.GetPokemonByName).Methods("GET")
 	r.HandleFunc("/pokemon/model", handlers.PokemonProbModel).Methods("GET")
 
+	client, err := handlers.NewMongoClient()
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB:", err)
+	}
+
+	r.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		handlers.RegisterUser(w, r, client)
+	}).Methods("POST")
+
+	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		handlers.LoginUser(w, r, client)
+	}).Methods("POST")
+
+	pokedexRouter := r.PathPrefix("/pokedex").Subrouter()
+	pokedexRouter.Use(handlers.AuthMiddleware)
+	pokedexRouter.HandleFunc("/add/{username}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.AddToInventory(w, r, client)
+	}).Methods("POST")
+	pokedexRouter.HandleFunc("/remove/{username}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.RemoveFromInventory(w, r, client)
+	}).Methods("DELETE")
+	pokedexRouter.HandleFunc("/display/{username}", func(w http.ResponseWriter, r *http.Request) {
+		handlers.GetUserInventory(w, r, client)
+	}).Methods("GET")
+
 	log.Println("Server started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
+
